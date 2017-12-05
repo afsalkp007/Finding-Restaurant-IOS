36fc3a5f0cc16bd74ac92af0b0c4697aa0d0262b
Randy <o1984531@gmail.com>
2017-12-05 12:24:07 +0800

    Add a LocationManager to handle authorization and location updating

--- Restaurant-Demo/Manager/LocationManager.swift
+++ Restaurant-Demo/Manager/LocationManager.swift
@@ -9,17 +9,89 @@
 import Foundation
 import CoreLocation
 
-class LocationManager {
-    private static var sLocationMgr:CLLocationManager?
+protocol LocationStatusDelegate {
+    func isLocationAuthorized(isAuthorized:Bool)
+    func didUpdateLocation(location:CLLocation)
+}
+
+class LocationManager: NSObject, CLLocationManagerDelegate {
     
-    func getInstance() -> CLLocationManager? {
+    private static var sInstance:LocationManager?
+    
+    private var mLocationMgr:CLLocationManager?
+    private var mDelegate:LocationStatusDelegate?
+    
+    static func getInstance() -> LocationManager? {
         
-        if LocationManager.sLocationMgr != nil {
-            return LocationManager.sLocationMgr
-        } else {
-            LocationManager.sLocationMgr = CLLocationManager()
-            return LocationManager.sLocationMgr
+        if sInstance == nil {
+            sInstance = LocationManager()
+            sInstance?.mLocationMgr = CLLocationManager()
+            sInstance?.mLocationMgr?.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
         }
+        return sInstance
+    }
+    
+    func setDelegate(delegate:LocationStatusDelegate?
+        ) {
+        self.mDelegate = delegate
+    }
+    
+    func isAuthorized() -> Bool {
+        return CLLocationManager.authorizationStatus() == .authorizedWhenInUse ||  CLLocationManager.authorizationStatus() == .authorizedAlways
+    }
+    
+    func requestAuthorize(authorizeStatus:CLAuthorizationStatus) {
+        switch (authorizeStatus) {
+        case .authorizedAlways:
+            self.mLocationMgr?.requestAlwaysAuthorization()
+        case .authorizedWhenInUse:
+            self.mLocationMgr?.requestWhenInUseAuthorization()
+        default:
+            return
+        }
+    }
+    
+    func requestLocationUpdate() -> Void {
+        self.mLocationMgr?.delegate = self
+        
+        if CLLocationManager.authorizationStatus() == .notDetermined {
+            // 1. 還沒有詢問過用戶以獲得權限
+            requestAuthorize(authorizeStatus:.authorizedWhenInUse)
+        } else if CLLocationManager.authorizationStatus() == .denied {
+            // 2. 用戶不同意
+            if self.mDelegate != nil {
+                self.mDelegate?.isLocationAuthorized(isAuthorized: false)
+            }
+        } else if CLLocationManager.authorizationStatus() == .authorizedWhenInUse || CLLocationManager.authorizationStatus() == .authorizedAlways {
+            self.mLocationMgr?.startUpdatingLocation()
+        }
+    }
+    
+    func stopLocationUpdate() {
+        self.mLocationMgr?.stopUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        
+        guard self.mDelegate != nil else {
+            return
+        }
+        
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse || CLLocationManager.authorizationStatus() == .authorizedAlways {
+            self.mDelegate?.isLocationAuthorized(isAuthorized: true)
+        } else {
+            self.mDelegate?.isLocationAuthorized(isAuthorized: false)
+        }
+    }
+    
+    public func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        guard self.mDelegate != nil else {
+            return
+        }
+        
+        let location = locations[0]
+        self.mDelegate?.didUpdateLocation(location: location)
     }
     
 }
