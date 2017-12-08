a93323093321c938a767deee27ffd9a33c29da16
Randy <o1984531@gmail.com>
2017-12-08 10:46:03 +0800

    Adjust the RestaurantDetailViewController
    1. Add api fetch flag in ViewDidAppear
    2. Check the is open now flag for displaying the OPEN/CLOSE Label
    3. Add the gesture action for Static Map for Navigation and Street View

--- Restaurant-Demo/View/Controller/RestaurantDetailViewController.swift
+++ Restaurant-Demo/View/Controller/RestaurantDetailViewController.swift
@@ -29,6 +29,7 @@ class RestaurantDetailViewController: UITableViewController, ApiCallback {
     private var mLoadingAlertController:UIAlertController?
     var mRestaurantSummaryInfo:YelpRestaruantSummaryInfo?
     var mRestaurantDetailInfo:YelpRestaruantDetailInfo?
+    private var mIsFirst = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,7 +39,10 @@ class RestaurantDetailViewController: UITableViewController, ApiCallback {
     }
     
     override func viewDidAppear(_ animated: Bool) {
-        fetchData()
+        if self.mIsFirst {
+            self.mIsFirst = false
+            fetchData()
+        }
     }
     
     func initView() {
@@ -50,7 +54,6 @@ class RestaurantDetailViewController: UITableViewController, ApiCallback {
         self.mIvStaticMapImageView.kf.setImage(with: URL(string: GoogleApiUtil.createStaticMapUrl(lat: lat!, lng: lng!, w: 200, h: 200)), placeholder:  #imageLiteral(resourceName: "no_image"))
     }
     
-    // MARK: - API Callback
     func fetchData() {
         if mRestaurantSummaryInfo == nil {
             return
@@ -80,12 +83,45 @@ class RestaurantDetailViewController: UITableViewController, ApiCallback {
         self.mIvRatingImage.image = self.mRestaurantDetailInfo?.getRatingImage(rating: self.mRestaurantDetailInfo?.rating ?? 0.0)
         self.mLbPriceLabel.text = self.mRestaurantDetailInfo?.price ?? ""
         self.mLbReviews.text = "\(self.mRestaurantDetailInfo?.review_count ?? 0) reviews"
-        self.mLbIsOpenStatusLabel.text = (self.mRestaurantDetailInfo?.hours![0].is_open_now)! ? "OPEN" : "CLOSE"
+        
+        self.mLbIsOpenStatusLabel.text = "N/A"
+        if let hours = self.mRestaurantDetailInfo?.hours, let isOpenNow = hours[0].is_open_now {
+            self.mLbIsOpenStatusLabel.text = (isOpenNow) ? "OPEN" : "CLOSE"
+        }
         
         for i in stride(from: 0, to: (self.mRestaurantDetailInfo?.photos?.count)!, by: 1) where i < self.mIvSubPhotos.count {
             self.mIvSubPhotos[i].kf.setImage(with: URL(string: (self.mRestaurantDetailInfo?.photos![i])!), placeholder: #imageLiteral(resourceName: "no_image"))
         }
     }
+    
+    
+    @IBAction func onStaticMapPressed(_ sender: Any) {
+        let lat = self.mRestaurantSummaryInfo?.coordinates?.latitude
+        let lng = self.mRestaurantSummaryInfo?.coordinates?.longitude
+        let alertController = UIAlertController(title: "Select a action", message: nil, preferredStyle: .actionSheet)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .destructive) { (action) in
+            self.dismiss(animated: true, completion: nil)
+        }
+        let navigationAction = UIAlertAction(title: "Navigation", style: .default) { (action) in
+            let url = URL(string: String(format:"http://maps.apple.com/?daddr=%f,%f&dirflg=d", lat!, lng!))
+            if #available(iOS 10.0, *) {
+                UIApplication.shared.open(url!, options: [:], completionHandler: nil)
+            } else {
+                UIApplication.shared.openURL(url!)
+            }
+        }
+        let streetViewAction = UIAlertAction(title: "Street View", style: .default) { (action) in
+            let panormaViewController = PanoramaViewController()
+            panormaViewController.mLat = lat!
+            panormaViewController.mLng = lng!
+            self.navigationController?.pushViewController(panormaViewController, animated: true)
+        }
+        alertController.addAction(navigationAction)
+        alertController.addAction(streetViewAction)
+        alertController.addAction(cancelAction)
+        present(alertController, animated: true, completion: nil)
+    }
+    
     
     func showLoadingDialog(loadingContent:String) {
         self.mLoadingAlertController = UIAlertController(title: nil, message: loadingContent, preferredStyle: .alert)
@@ -105,6 +141,8 @@ class RestaurantDetailViewController: UITableViewController, ApiCallback {
             self.mLoadingAlertController = nil
         }
     }
+    
+    // MARK: - API Callback
     
     func onError(apiTag: String, errorMsg: String) {
         closeLoadingDialog()
