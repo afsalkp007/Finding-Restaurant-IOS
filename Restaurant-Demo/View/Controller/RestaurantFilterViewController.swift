1cecbf6893f5d8322da275c9348b889240ab7999
Randy <o1984531@gmail.com>
2017-12-10 10:46:55 +0800

    Add the layout in IB for RestaurantFilterViewController

--- Restaurant-Demo/View/Controller/RestaurantFilterViewController.swift
+++ Restaurant-Demo/View/Controller/RestaurantFilterViewController.swift
@@ -2,36 +2,27 @@
 //  RestaurantFilterViewController.swift
 //  Restaurant-Demo
 //
-//  Created by yomi on 2017/12/9.
+//  Created by yomi on 2017/12/10.
 //  Copyright © 2017年 yomi. All rights reserved.
 //
 
 import UIKit
 
-class RestaurantFilterViewController: UIViewController {
+class RestaurantFilterViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        initView()
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    func initView() {
-    }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
