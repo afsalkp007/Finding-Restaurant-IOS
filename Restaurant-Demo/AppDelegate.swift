1ed5fda67c2577859e33fcdd9752553f72613893
Randy <o1984531@gmail.com>
2017-12-20 09:04:32 +0800

    Add .gitignore file

--- Restaurant-Demo/AppDelegate.swift
+++ Restaurant-Demo/AppDelegate.swift
@@ -21,7 +21,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
-//        UIApplication.shared.isStatusBarHidden = false
+//      UIApplication.shared.isStatusBarHidden = false
         
         /* Init firebase configs */
         FirebaseApp.configure()
