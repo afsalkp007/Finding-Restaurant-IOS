07b01e4e15cdfc73add5d7534810ca63ab207b48
Randy <o1984531@gmail.com>
2018-02-11 22:36:04 +0800

    Remove useless files and adjust class name

--- Restaurant-Demo/View/Cell/ReviewSummaryItemTableViewCell.swift
+++ Restaurant-Demo/View/Cell/ReviewSummaryItemTableViewCell.swift
@@ -8,9 +8,7 @@
 
 import UIKit
 
-class ReviewDetailItemTableViewCell: UITableViewCell {
-
-    
+class ReviewSummaryItemTableViewCell: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
