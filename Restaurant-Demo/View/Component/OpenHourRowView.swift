4d125c3c0b01c17a9b5d2f9622b8f652c0008ba5
Randy <o1984531@gmail.com>
2018-02-17 00:16:19 +0800

    Add OpenHourRow item view

---
+++ Restaurant-Demo/View/Component/OpenHourRowView.swift
@@ -0,0 +1,21 @@
+//
+//  OpenHourRowView.swift
+//  Restaurant-Demo
+//
+//  Created by yomi on 2018/2/16.
+//  Copyright © 2018年 yomi. All rights reserved.
+//
+
+import UIKit
+
+class OpenHourRowView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
