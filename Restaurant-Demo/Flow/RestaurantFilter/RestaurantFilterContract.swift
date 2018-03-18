f9cff6e254af386b839c73d119832b0cca4e00f8
Randy <o1984531@gmail.com>
2018-03-18 22:26:34 +0800

    Remove useless comment

--- Restaurant-Demo/Flow/RestaurantFilter/RestaurantFilterContract.swift
+++ Restaurant-Demo/Flow/RestaurantFilter/RestaurantFilterContract.swift
@@ -3,7 +3,6 @@ import UIKit
 
 protocol RestaurantFilterPresenterProtocol{
     
-    
     func onViewDidLoad()
     func onViewDidAppear()
     func attachView(view:RestaurantFilterViewProtocol)
@@ -11,24 +10,12 @@ protocol RestaurantFilterPresenterProtocol{
     func onApplyPressed(price:Int, openAt:Int, sortingRuleIndex:Int)    
     func onCancelPressed()
     
-//    func onInitParameters(summaryInfo:YelpRestaruantSummaryInfo?)
-    
-//    func onReviewItemSelect(reviewDetail:YelpReviewDetailInfo)
-    
 }
 
 protocol RestaurantFilterViewProtocol{
-//    func refreshBasicInfo(summaryInfo:YelpRestaruantSummaryInfo?, detailInfo:YelpRestaruantDetailInfo?)
-//    func refreshReviewInfo(reviews:[YelpReviewDetailInfo]?)
-//
-//    func doPresent(_ viewControllerToPresent: UIViewController, animated flag: Bool, completion: (() -> Swift.Void)?)
-//    func doDismiss(animated flag: Bool, completion: (() -> Swift.Void)?)
-//
+    
     func refreshFilterConfig(config:FilterConfigs?)
     
     func doPerformSegue(withIdentifier identifier: String, sender: Any?)
-//
-//    func showLoading(loadingContent:String)
-//    func closeLoading()
 }
 
